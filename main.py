1cb1dd5d6612a0b138337a9e1be2cef3c03b01af
Chandan Mohan <chandanmohan@Chandans-MacBook-Air.local>
2025-04-05 14:56:27 -0400

    updated main.py

--- main.py
+++ main.py
@@ -20,28 +20,16 @@ from scrapers.uber import fetch_jobs as fetch_uber_jobs
 from scrapers.linkedin import fetch_linkedin_jobs
 
 SCRAPERS = [
-    fetch_servicenow_jobs,
-    fetch_adobe_jobs,
-    fetch_apple_jobs,
-    fetch_nvidia_jobs,
-    fetch_paypal_jobs,
-    fetch_uber_jobs,
+    # fetch_servicenow_jobs,
+    # fetch_adobe_jobs,
+    # fetch_apple_jobs,
+    # fetch_nvidia_jobs,
+    # fetch_paypal_jobs,
+    # fetch_uber_jobs,
     lambda: fetch_linkedin_jobs(
-        "https://www.linkedin.com/jobs/search?keywords=Software%20Engineer&location=San%20Jose&geoId=106233382&distance=50&f_TPR=r3600&position=1&pageNum=0", 
-        "linkedin_software_engineer"
-    ),
-     lambda: fetch_linkedin_jobs(
-        "https://www.linkedin.com/jobs/search?keywords=Full%20Stack&location=San%20Jose&geoId=106233382&distance=25&f_TPR=r3600&position=1&pageNum=0", 
-        "linkedin_full_stack"
-    ),
-    lambda: fetch_linkedin_jobs(
-        "https://www.linkedin.com/jobs/search?keywords=frontend&location=San%20Jose&geoId=106233382&distance=25&f_TPR=r3600&position=1&pageNum=0", 
-        "linkedin_frontend"
-    ),
-    lambda: fetch_linkedin_jobs(
-        "https://www.linkedin.com/jobs/search?keywords=mobile&location=San%20Jose&geoId=106233382&distance=25&f_TPR=r3600&position=1&pageNum=0", 
-        "linkedin_mobile"
-    ),
+        "https://www.linkedin.com/jobs/search/?currentJobId=4202324781&f_E=2%2C3&f_TPR=r3600&keywords=bigdata%20engineer&origin=JOB_SEARCH_PAGE_JOB_FILTER", 
+        "linkedin_Bigdata_engineer"
+    )
 ]
 
 def main():
